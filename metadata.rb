a0d9581dae5162eba368bd031c2de02c733e17fc
Faizal Zakaria <phaibusiness@gmail.com>
2016-03-13 20:31:03 +0800

    Typo on apt_resource

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'phaibusiness@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures chef_newrelic_server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 %w{redhat centos debian ubuntu amazon}.each do |os|
   supports os
--- recipes/default.rb
+++ recipes/default.rb
@@ -21,7 +21,7 @@ when "debian", "ubuntu"
   # Apt
   include_recipe 'apt'
 
-  apt_resource 'newrelic' do
+  apt_repository 'newrelic' do
     uri 'http://apt.newrelic.com/debian/'
     distribution 'newrelic'
     components [ 'non-free' ]
